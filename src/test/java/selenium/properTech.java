e174687c59ddef4ec8d5d8fc47286f7abc1eb75f
prabhusundaram28 <prabhusundaram28@gmail.com>
2020-08-09 14:27:04 +0530

    perfect task

---
+++ src/test/java/selenium/properTech.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class properTech {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+	}
+
+}
